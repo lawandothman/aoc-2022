a86dca429c1cdb3d37181c64eba3ec081aafe9a9
Lawand Othman <lwnd@pm.me>
2022-12-02 06:43:55 +0000

    part two

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -2,12 +2,13 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut score = 0u32;
     for game in input.lines() {
         let mut choices = game.split_whitespace();
+
         let opponent = choices.next().unwrap();
         let player = choices.next().unwrap();
 
-        // X - A rock
-        // Y - B paper
-        // Z - C scissor
+        // A - X rock
+        // B - Y paper
+        // C - Z scissor
 
         score += match player {
             "X" => {
@@ -41,7 +42,46 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut score = 0u32;
+    for game in input.lines() {
+        let mut choices = game.split_whitespace();
+
+        let opponent = choices.next().unwrap();
+        let player = choices.next().unwrap();
+
+        // A - X rock - loss
+        // B - Y paper - draw
+        // C - Z scissor - win
+
+        score += match player {
+            "X" => {
+                0 + match opponent {
+                    "A" => 3,
+                    "B" => 1,
+                    "C" => 2,
+                    _ => panic!("Unsupported opponent choice"),
+                }
+            }
+            "Y" => {
+                3 + match opponent {
+                    "A" => 1,
+                    "B" => 2,
+                    "C" => 3,
+                    _ => panic!("Unsupported opponent choice"),
+                }
+            }
+            "Z" => {
+                6 + match opponent {
+                    "A" => 2,
+                    "B" => 3,
+                    "C" => 1,
+                    _ => panic!("Unsupported opponent choice"),
+                }
+            }
+            _ => panic!("Unsupported player choice"),
+        }
+    }
+    Some(score)
 }
 
 fn main() {
@@ -63,6 +103,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
