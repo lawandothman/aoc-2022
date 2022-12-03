b7c15d900e0ba8552f437045b84f6038475b65cd
Lawand Othman <lwnd@pm.me>
2022-12-03 07:49:28 +0000

    part two

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -12,11 +12,27 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|sack| sack.chars())
+            .map(|sack| sack.map(get_priority).collect_vec())
+            .chunks(3)
+            .into_iter()
+            .map(|group| group.collect_vec())
+            .map(|sack| get_badge((&sack[0], &sack[1], &sack[2])))
+            .sum::<u32>(),
+    )
 }
 
 fn compartmentalize((a, b): (&[u32], &[u32])) -> u32 {
     *a.into_iter().find(|x| b.contains(x)).unwrap()
+}
+
+fn get_badge((a, b, c): (&[u32], &[u32], &[u32])) -> u32 {
+    *a.into_iter()
+        .find(|x| b.contains(x) && c.contains(x))
+        .unwrap()
 }
 
 fn get_priority(item_type: char) -> u32 {
@@ -46,6 +62,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(70));
     }
 }
