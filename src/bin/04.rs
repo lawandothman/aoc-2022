416aa1b92aad50461d495b6ebc2e0757653545f8
Lawand Othman <lwnd@pm.me>
2022-12-04 09:28:53 +0000

    part one

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,5 +1,30 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .lines()
+            .filter(|line| {
+                let assignments = line
+                    .split(',')
+                    .map(|assignment| {
+                        assignment
+                            .split('-')
+                            .map(|n| n.parse::<u32>().unwrap())
+                            .collect::<Vec<u32>>()
+                    })
+                    .collect::<Vec<Vec<u32>>>();
+
+                let x1 = assignments[0][0];
+                let x2 = assignments[0][1];
+
+                let y1 = assignments[1][0];
+                let y2 = assignments[1][1];
+
+                (x1 >= y1 && x2 <= y2) || (y1 >= x1 && y2 <= x2)
+            })
+            .count()
+            .try_into()
+            .unwrap(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -19,7 +44,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
