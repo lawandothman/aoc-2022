8ea2a443eecf5ea13b4a02e4cd2080f22ff65548
Lawand Othman <lwnd@pm.me>
2022-12-05 19:23:09 +0000

    part one

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,5 +1,53 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
+fn parse_input(input: &str) -> (Vec<VecDeque<char>>, Vec<Vec<usize>>) {
+    let (crates, moves) = input.split("\n\n").into_iter().collect_tuple().unwrap();
+
+    let crates = crates
+        .lines()
+        .flat_map(|l| {
+            l.chars()
+                .skip(1)
+                .step_by(4)
+                .enumerate()
+                .filter(|(_, ch)| ch.is_alphanumeric())
+        })
+        .into_grouping_map()
+        .collect::<VecDeque<char>>()
+        .into_iter()
+        .sorted_by_key(|(idx, _)| *idx)
+        .map(|(_, stack)| stack)
+        .collect::<Vec<VecDeque<char>>>();
+
+    let moves = moves
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+
+    (crates, moves)
+}
+
+pub fn part_one(input: &str) -> Option<String> {
+    let (mut crates, moves) = parse_input(input);
+
+    for m in moves {
+        for _ in 0..m[0] {
+            let item = crates[m[1] - 1].pop_front().unwrap();
+            crates[m[2] - 1].push_front(item);
+        }
+    }
+    Some(
+        crates
+            .into_iter()
+            .filter_map(|mut stack| stack.pop_front())
+            .collect::<String>(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -19,7 +67,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(String::from("CMZ")));
     }
 
     #[test]
