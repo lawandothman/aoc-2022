7a89961fceb867024a06b753e7337525e888a2f1
Lawand Othman <lwnd@pm.me>
2022-12-09 15:08:32 +0000

    part one

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,62 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+#[macro_export]
+macro_rules! tree {
+    ($x: ident, $y: ident, $dir_value: ident, $dir_max: ident, $seen: ident) => {
+        if $dir_value > $dir_max {
+            $seen[$y][$x] += 1;
+            $dir_max = $dir_value;
+        }
+    };
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let trees = input
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|line| {
+            line.chars()
+                .filter_map(|x| x.to_digit(10))
+                .map(|x| x as isize)
+                .collect::<Vec<isize>>()
+        })
+        .collect::<Vec<Vec<isize>>>();
+
+    let h = trees.len();
+    let w = trees[0].len();
+    let mut seen = vec![vec![0isize; w]; h];
+
+    for y in 0..h {
+        let mut east_h = -1;
+        let mut west_h = -1;
+        for x in 0..w {
+            let w_idx = w - x - 1;
+
+            let west = trees[y][w_idx];
+            let east = trees[y][x];
+
+            tree!(x, y, east, east_h, seen);
+            tree!(w_idx, y, west, west_h, seen);
+        }
+    }
+    for x in 0..w {
+        let mut north_h = -1;
+        let mut south_h = -1;
+        for y in 0..h {
+            let n_idx = h - y - 1;
+
+            let north = trees[n_idx][x];
+            let south = trees[y][x];
+
+            tree!(x, y, south, south_h, seen);
+            tree!(x, n_idx, north, north_h, seen);
+        }
+    }
+
+    Some(
+        seen.iter()
+            .flat_map(|x| x.iter())
+            .filter(|x| **x != 0)
+            .count(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -19,7 +76,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 8);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(21));
     }
 
     #[test]
