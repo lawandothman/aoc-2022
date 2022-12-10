42157bbed417e680ea1fc5f229da0bc8ce276ef9
Lawand Othman <lwnd@pm.me>
2022-12-10 00:21:15 +0000

    part two

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -59,8 +59,76 @@ pub fn part_one(input: &str) -> Option<usize> {
     )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let trees = input
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|line| {
+            line.chars()
+                .filter_map(|x| x.to_digit(10))
+                .map(|x| x as isize)
+                .collect::<Vec<isize>>()
+        })
+        .collect::<Vec<Vec<isize>>>();
+
+    let h = trees.len();
+    let w = trees[0].len();
+    let mut max_score = 0;
+
+    for y in 1..(w - 1) {
+        for x in 1..(h - 1) {
+            let height = trees[y][x];
+            let mut score = 1;
+
+            score *= if let Some((pos, _)) = trees[y][0..x]
+                .iter()
+                .rev()
+                .enumerate()
+                .find(|(_, f)| **f >= height)
+            {
+                pos + 1
+            } else {
+                x
+            };
+
+            score *= if let Some((pos, _)) = trees[y][(x + 1)..]
+                .iter()
+                .enumerate()
+                .find(|(_, f)| **f >= height)
+            {
+                pos + 1
+            } else {
+                w - x - 1
+            };
+
+            score *= if let Some((pos, _)) = trees[0..y]
+                .iter()
+                .rev()
+                .enumerate()
+                .find(|(_, r)| r[x] >= height)
+            {
+                pos + 1
+            } else {
+                y
+            };
+
+            score *= if let Some((pos, _)) = trees[(y + 1)..]
+                .iter()
+                .enumerate()
+                .find(|(_, r)| r[x] >= height)
+            {
+                pos + 1
+            } else {
+                h - y - 1
+            };
+
+            if score > max_score {
+                max_score = score
+            }
+        }
+    }
+
+    Some(max_score)
 }
 
 fn main() {
