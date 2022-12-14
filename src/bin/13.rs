aff209a4329f5a93687c53c5e4ca6f2c3716f493
Lawand Othman <lwnd@pm.me>
2022-12-14 18:34:24 +0000

    🎄 Day 13 🎄 (#15)
    * scaffold
    
    * part one
    
    * part two

---
+++ src/bin/13.rs
@@ -0,0 +1,133 @@
+use std::{cmp::Ordering, str::FromStr};
+
+#[derive(Eq)]
+enum Packet {
+    Int(u8),
+    List(Vec<Packet>),
+}
+
+impl FromStr for Packet {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = &s[1..s.len() - 1];
+        let mut int = None;
+        let mut stack = Vec::new();
+        let mut list = Vec::new();
+
+        for c in s.bytes() {
+            match c {
+                b'0'..=b'9' => {
+                    int = Some(match int.take() {
+                        Some(val) => val * 10 + (c - b'0'),
+                        None => c - b'0',
+                    })
+                }
+                b',' => {
+                    if let Some(val) = int.take() {
+                        list.push(Packet::Int(val))
+                    }
+                }
+                b'[' => {
+                    stack.push((list, int));
+                    list = Vec::new();
+                    int = None;
+                }
+
+                b']' => {
+                    if let Some(val) = int.take() {
+                        list.push(Packet::Int(val));
+                    }
+                    let packet = Packet::List(list);
+                    (list, int) = stack.pop().unwrap();
+                    list.push(packet)
+                }
+
+                _ => unreachable!(),
+            }
+        }
+        if let Some(val) = int.take() {
+            list.push(Packet::Int(val));
+        }
+        Ok(Packet::List(list))
+    }
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Packet::Int(x), Packet::Int(y)) => x.cmp(y),
+            (Packet::List(x), Packet::List(y)) => x.cmp(y),
+            (x @ Packet::Int(_), Packet::List(y)) => std::slice::from_ref(x).cmp(y.as_slice()),
+            (Packet::List(x), y @ Packet::Int(_)) => x.as_slice().cmp(std::slice::from_ref(y)),
+        }
+    }
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn parse_packets(input: &str) -> Vec<Packet> {
+    input
+        .lines()
+        .map(|x| x.trim())
+        .filter(|l| !l.is_empty())
+        .map(|x| x.parse::<Packet>().unwrap())
+        .collect::<Vec<Packet>>()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(
+        parse_packets(input)
+            .chunks(2)
+            .enumerate()
+            .filter_map(
+                |(i, chunk)| match chunk[0].cmp(&chunk[1]) == Ordering::Less {
+                    true => Some(i + 1),
+                    false => None,
+                },
+            )
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut packets = parse_packets(input);
+    packets.sort_unstable();
+    let x = packets.binary_search(&Packet::Int(2)).unwrap_err() + 1;
+    let y = packets.binary_search(&Packet::Int(6)).unwrap_err() + 2;
+
+    Some(x * y)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 13);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 13);
+        assert_eq!(part_one(&input), Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 13);
+        assert_eq!(part_two(&input), Some(140));
+    }
+}
