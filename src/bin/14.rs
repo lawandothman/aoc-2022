6cf7030b2802fb7d774ad8a52bf7f82893662d83
Lawand Othman <lwnd@pm.me>
2022-12-21 00:46:12 +0000

    day 14 solution

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,9 +1,89 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::{cmp, collections::HashSet};
+
+fn parse_input(input: &str) -> (HashSet<(usize, usize)>, usize) {
+    let mut blocked = HashSet::new();
+    let mut floor = 0;
+
+    for line in input.lines() {
+        line.split(" -> ")
+            .fold(None, |acc: Option<(usize, usize)>, str| {
+                let (x, y) = str
+                    .split_once(',')
+                    .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap()))
+                    .unwrap();
+                floor = cmp::max(floor, y);
+
+                if let Some(last) = acc {
+                    if x == last.0 {
+                        for i in cmp::min(y, last.1)..=cmp::max(y, last.1) {
+                            blocked.insert((x, i));
+                        }
+                    } else if y == last.1 {
+                        for i in cmp::min(x, last.0)..=cmp::max(x, last.0) {
+                            blocked.insert((i, y));
+                        }
+                    }
+                } else {
+                    blocked.insert((x, y));
+                }
+                Some((x, y))
+            });
+    }
+
+    (blocked, floor)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<usize> {
+    let has_floor = false;
+    let (mut blocked, floor) = parse_input(input);
+    let count = blocked.len();
+    let mut q = Vec::new();
+    q.push((500, 0));
+
+    while let Some(&(x, y)) = q.last() {
+        let mut found = false;
+        if y <= floor {
+            for next in [(x + 1, y + 1), (x - 1, y + 1), (x, y + 1)]
+                .into_iter()
+                .filter(|next| !blocked.contains(next))
+            {
+                q.push(next);
+                found = true;
+            }
+        }
+        if !found {
+            blocked.insert((x, y));
+            q.pop();
+        } else if y >= floor {
+            break;
+        }
+    }
+    Some(blocked.len() - count)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let (mut blocked, floor) = parse_input(input);
+    let count = blocked.len();
+    let mut q = Vec::new();
+    q.push((500, 0));
+
+    while let Some(&(x, y)) = q.last() {
+        let mut found = false;
+        if y <= floor {
+            for next in [(x + 1, y + 1), (x - 1, y + 1), (x, y + 1)]
+                .into_iter()
+                .filter(|next| !blocked.contains(next))
+            {
+                q.push(next);
+                found = true;
+            }
+        }
+        if !found {
+            blocked.insert((x, y));
+            q.pop();
+        }
+    }
+    Some(blocked.len() - count)
 }
 
 fn main() {
@@ -19,12 +99,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 14);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 14);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(93));
     }
 }
